9d63696fd7aa116d5f86b6aa4636dffd6cb241af
zJanet <siwei@connect.hku.hk>
2019-06-14 14:27:33 +0800

    Updates

---
+++ precache-manifest.2685b453abd44260a9cd0fb6151d934f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0da4f1b93a8cacb0a8ea771c8b2d6d44",
+    "url": "/react-tutorial/index.html"
+  },
+  {
+    "revision": "e6aeeee42585cb03de7c",
+    "url": "/react-tutorial/static/css/main.682ca21c.chunk.css"
+  },
+  {
+    "revision": "3a9e33913cab0720716d",
+    "url": "/react-tutorial/static/js/2.86a2265c.chunk.js"
+  },
+  {
+    "revision": "e6aeeee42585cb03de7c",
+    "url": "/react-tutorial/static/js/main.82ba7c89.chunk.js"
+  },
+  {
+    "revision": "2a4d04de927634e03e5b",
+    "url": "/react-tutorial/static/js/runtime~main.782a6e4d.js"
+  }
+]);
